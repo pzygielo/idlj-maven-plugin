f8fd7619ad8c45f7f33ff72f2af2bc58585a5ab2
Stefano Lenzi <kismet.sl@gmail.com>
2007-07-06 17:20:43 +0000

    Fixed JIRA bug issue MOJO-834 Fixed most of the checkstyle issues
    git-svn-id: file:///mojohaus-svn-repo/trunk/mojo/idlj-maven-plugin@4513 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/idlj/JacorbTranslator.java
+++ src/main/java/org/codehaus/mojo/idlj/JacorbTranslator.java
@@ -1,5 +1,24 @@
 package org.codehaus.mojo.idlj;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file 
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied.  See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -19,45 +38,86 @@ import java.util.List;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.logging.Log;
 
+/**
+ * This class implement the <code>CompilerTranslator</code> for the
+ * JacORB IDL compiler
+ * 
+ * @author Anders Hessellund Jensen <ahj@trifork.com>
+ * @version $Id$
+ */
+public class JacorbTranslator implements CompilerTranslator
+{
 
-public class JacorbTranslator implements CompilerTranslator {
-
+    /**
+     * enable/disable debug messages 
+     */
     private final boolean debug;
-    private final Log log;
 
-    public JacorbTranslator(boolean debug, Log log) {
-        this.debug = debug;
-        this.log = log;
+    /**
+     * the <code>Log</code> that will used for the messages
+     */
+    private Log log;
+
+    /**
+     * Create an <code>JacorbTranslator</code> object with the specified <code>Log</code> and
+     * enable or disable the debugging
+     * 
+     * @param d <code>true</code> to enable debug logging
+     * @param l <code>Log</code> to use for logging the activity
+     */
+    public JacorbTranslator( boolean d, Log l )
+    {
+        debug = d;
+        log = l;
     }
 
-    private void invokeCompiler(Class compilerClass, List args) throws MojoExecutionException {
+    /**
+     * 
+     * Invoke the specified compiler with a set of arguments
+     * 
+     * @param compilerClass the <code>Class</code> that implemtns the compiler
+     * @param args a <code>List</code> that contains the arguments to use for the compiler
+     * @throws MojoExecutionException if the compilation fail or the compiler crashes
+     */
+    private void invokeCompiler( Class compilerClass, List args ) throws MojoExecutionException
+    {
         // It would be great to use some 3rd party library for this stuff
         boolean fork = true;
-        if (!fork) {
-            try {
-                String arguments[] = (String[]) args.toArray(new String[args.size()]);
+        if ( !fork )
+        {
+            try
+            {
+                String arguments[] = (String[]) args.toArray( new String[args.size()] );
 
-                if (debug) {
+                if ( debug )
+                {
                     String command = "compile";
-                    for (int i = 0; i < arguments.length; i++) {
+                    for ( int i = 0; i < arguments.length; i++ )
+                    {
                         command += " " + arguments[i];
                     }
-                    log.info(command);
+                    log.info( command );
                 }
 
-                Method compileMethod = compilerClass.getMethod("compile", new Class[]{String[].class});
-                compileMethod.invoke(compilerClass, arguments);
-            } catch (InvocationTargetException e) {
-                throw new MojoExecutionException("Compilation failed", e.getTargetException());
-            } catch (Throwable t) {
-                throw new MojoExecutionException("Compilation failed", t);
+                Method compileMethod = compilerClass.getMethod( "compile", new Class[] { String[].class } );
+                compileMethod.invoke( compilerClass, arguments );
             }
-        } else {
+            catch ( InvocationTargetException e )
+            {
+                throw new MojoExecutionException( "Compilation failed", e.getTargetException() );
+            }
+            catch ( Throwable t )
+            {
+                throw new MojoExecutionException( "Compilation failed", t );
+            }
+        }
+        else
+        {
 
             // Forks a new java process.
             // Get path to java binary
-            File javaHome = new File(System.getProperty("java.home"));
-            File javaBin = new File(new File(javaHome, "bin"), "java");
+            File javaHome = new File( System.getProperty( "java.home" ) );
+            File javaBin = new File( new File( javaHome, "bin" ), "java" );
 
             // Get current class path
             URLClassLoader cl = (URLClassLoader) this.getClass().getClassLoader();
@@ -67,136 +127,196 @@ public class JacorbTranslator implements CompilerTranslator {
             List binArgs = new ArrayList();
 
             // First argument is the java binary to run
-            binArgs.add(javaBin.getPath());
+            binArgs.add( javaBin.getPath() );
 
             // Add the classpath to argument list
-            binArgs.add("-classpath");
-            String classPath = "" + new File(classPathUrls[0].getPath().replaceAll("%20", " "));
-            for (int i = 1; i < classPathUrls.length; i++) {
-                classPath += File.pathSeparator + new File(classPathUrls[i].getPath() .replaceAll("%20", " "));
+            binArgs.add( "-classpath" );
+            String classPath = "" + new File( classPathUrls[0].getPath().replaceAll( "%20", " " ) );
+            for ( int i = 1; i < classPathUrls.length; i++ )
+            {
+                classPath += File.pathSeparator + new File( classPathUrls[i].getPath().replaceAll( "%20", " " ) );
             }
             classPath += "";
-            binArgs.add(classPath);
+            binArgs.add( classPath );
 
             // Add class containing main method to arg list
-            binArgs.add(compilerClass.getName());
+            binArgs.add( compilerClass.getName() );
 
             // Add java arguments
-            for (Iterator it = args.iterator(); it.hasNext();) {
+            for ( Iterator it = args.iterator(); it.hasNext(); )
+            {
                 Object o = it.next();
-                binArgs.add(o.toString());
+                binArgs.add( o.toString() );
             }
 
             // Convert arg list to array
             String[] argArray = new String[binArgs.size()];
-            for (int i = 0; i < argArray.length; i++) {
-                argArray[i] = binArgs.get(i).toString();
+            for ( int i = 0; i < argArray.length; i++ )
+            {
+                argArray[i] = binArgs.get( i ).toString();
             }
 
-            if (debug) {
+            if ( debug )
+            {
                 String command = "";
-                for (int i = 0; i < argArray.length; i++) {
+                for ( int i = 0; i < argArray.length; i++ )
+                {
                     command += " " + argArray[i];
                 }
-                log.info(command);
+                log.info( command );
             }
 
-            try {
-                Process p = Runtime.getRuntime().exec(argArray);
-                redirectStream(p.getErrorStream(), System.err, "");
-                redirectStream(p.getInputStream(), System.out, "");
+            try
+            {
+                Process p = Runtime.getRuntime().exec( argArray );
+                redirectStream( p.getErrorStream(), System.err, "" );
+                redirectStream( p.getInputStream(), System.out, "" );
 
                 p.waitFor();
 
-                if (p.exitValue() != 0) {
-                    throw new MojoExecutionException("IDL Compilation failure");
+                if ( p.exitValue() != 0 )
+                {
+                    throw new MojoExecutionException( "IDL Compilation failure" );
                 }
-            } catch (IOException e) {
-                throw new MojoExecutionException("Error forking compiler", e);
-            } catch (InterruptedException e) {
-                throw new MojoExecutionException("Thread interrupted unexpectedly", e);
+            }
+            catch ( IOException e )
+            {
+                throw new MojoExecutionException( "Error forking compiler", e );
+            }
+            catch ( InterruptedException e )
+            {
+                throw new MojoExecutionException( "Thread interrupted unexpectedly", e );
             }
         }
     }
 
-    public void invokeCompiler(Log log, String sourceDirectory, String targetDirectory, String idlFile, Source source) throws MojoExecutionException {
+    /**
+     * This method it's used to invoke the compiler
+     * 
+     * @param l the<code>Log</code> to use for log messages
+     * @param sourceDirectory the path to the sources
+     * @param includeDirs the <code>List</code> of directories where to find the includes
+     * @param targetDirectory the path to the destination of the compilation
+     * @param idlFile the path to the file to compile
+     * @param source //TODO ???
+     * @throws MojoExecutionException the exeception is thrown whenever the compilation fails or crashes
+     * 
+     * @see CompilerTranslator#invokeCompiler(Log, String, List, String, String, Source)
+     */
+    public void invokeCompiler( Log l, String sourceDirectory, List includeDirs, String targetDirectory,
+            String idlFile, Source source ) throws MojoExecutionException
+    {
+        log = l;
+        
         List args = new ArrayList();
 
         // TODO: This should be configurable
-        args.add("-sloppy_names");
+        args.add( "-sloppy_names" );
 
-        args.add("-I" + sourceDirectory);
-        args.add("-d");
-        args.add(targetDirectory);
+        args.add( "-I" + sourceDirectory );
 
-        if (source.emitSkeletons() != null &&
-            !source.emitSkeletons().booleanValue()) {
-            args.add("-noskel");
-        }
-        if (source.emitStubs() != null &&
-            !source.emitStubs().booleanValue()) {
-            args.add("-nostub");
-        }
-
-        if (source.getPackagePrefix() != null) {
-            args.add("-i2jpackage");
-            args.add(":" + source.getPackagePrefix());
-        }
-
-        if (source.getPackagePrefixes() != null) {
-            for (Iterator prefixes = source.getPackagePrefixes().iterator(); prefixes.hasNext();) {
-                PackagePrefix prefix = (PackagePrefix) prefixes.next();
-                args.add("-i2jpackage");
-                args.add(prefix.getType() + ":" + prefix.getPrefix() + "." + prefix.getType());
+        // add idl files from other directories as well
+        if ( includeDirs != null )
+        {
+            Iterator iter = includeDirs.iterator();
+            while ( iter.hasNext() )
+            {
+                String includeDir = (String) iter.next();
+                args.add( "-I" + includeDir );
             }
         }
 
-        if (source.getDefines() != null) {
-            for (Iterator defs = source.getDefines().iterator(); defs.hasNext();) {
+        args.add( "-d" );
+        args.add( targetDirectory );
+
+        if ( source.emitSkeletons() != null && !source.emitSkeletons().booleanValue() )
+        {
+            args.add( "-noskel" );
+        }
+        if ( source.emitStubs() != null && !source.emitStubs().booleanValue() )
+        {
+            args.add( "-nostub" );
+        }
+
+        if ( source.getPackagePrefix() != null )
+        {
+            args.add( "-i2jpackage" );
+            args.add( ":" + source.getPackagePrefix() );
+        }
+
+        if ( source.getPackagePrefixes() != null )
+        {
+            for ( Iterator prefixes = source.getPackagePrefixes().iterator(); prefixes.hasNext(); )
+            {
+                PackagePrefix prefix = (PackagePrefix) prefixes.next();
+                args.add( "-i2jpackage" );
+                args.add( prefix.getType() + ":" + prefix.getPrefix() + "." + prefix.getType() );
+            }
+        }
+
+        if ( source.getDefines() != null )
+        {
+            for ( Iterator defs = source.getDefines().iterator(); defs.hasNext(); )
+            {
                 Define define = (Define) defs.next();
                 String arg = "-D" + define.getSymbol();
-                if (define.getValue() != null) {
+                if ( define.getValue() != null )
+                {
                     arg += "=" + define.getValue();
                 }
-                args.add(arg);
+                args.add( arg );
             }
         }
 
-        if (source.getAdditionalArguments() != null) {
-            for (Iterator it = source.getAdditionalArguments().iterator(); it.hasNext();) {
-                args.add(it.next());
+        if ( source.getAdditionalArguments() != null )
+        {
+            for ( Iterator it = source.getAdditionalArguments().iterator(); it.hasNext(); )
+            {
+                args.add( it.next() );
             }
         }
 
-        args.add(idlFile);
+        args.add( idlFile );
 
         Class compilerClass;
-        try {
-            compilerClass = Class.forName("org.jacorb.idl.parser");
-        } catch (ClassNotFoundException e) {
-            throw new MojoExecutionException("JacORB IDL compiler not found", e);
+        try
+        {
+            compilerClass = Class.forName( "org.jacorb.idl.parser" );
+        }
+        catch ( ClassNotFoundException e )
+        {
+            throw new MojoExecutionException( "JacORB IDL compiler not found", e );
         }
 
-        invokeCompiler(compilerClass, args);
+        invokeCompiler( compilerClass, args );
     }
 
-    public static void redirectStream(final InputStream in,
-                                      final OutputStream out, final String streamName) {
+    /**
+     * This methos it's used to redirect an <code>InputeStream</code> to a
+     * <code>OutputStream</code>
+     * 
+     * @param in the <code>InputStream</code> to read from
+     * @param out the <code>OutputStream</code> to write into
+     * @param streamName the name of Stream
+     */
+    public static void redirectStream( final InputStream in, final OutputStream out, final String streamName )
+    {
         Thread stdoutTransferThread = new Thread() {
-            public void run() {
-                PrintWriter pw = new PrintWriter(new OutputStreamWriter(out),
-                                                 true);
-                try {
-                    BufferedReader reader = new BufferedReader(
-                            new InputStreamReader(in));
+            public void run()
+            {
+                PrintWriter pw = new PrintWriter( new OutputStreamWriter( out ), true );
+                try
+                {
+                    BufferedReader reader = new BufferedReader( new InputStreamReader( in ) );
                     String line;
-                    while ((line = reader.readLine()) != null) {
-
-                        pw.println(line);
+                    while ( ( line = reader.readLine() ) != null )
+                    {
+                        pw.println( line );
                     }
-                } catch (Throwable e) {
+                }
+                catch ( Throwable e )
+                {
                     e.printStackTrace();
-                    // throw new Error(e);
                 }
             }
         };
--- src/main/java/org/codehaus/mojo/idlj/Source.java
+++ src/main/java/org/codehaus/mojo/idlj/Source.java
@@ -1,73 +1,82 @@
 package org.codehaus.mojo.idlj;
-/* *
- *
- * Copyright 2005 (C) The original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file 
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, 
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY 
+ * KIND, either express or implied.  See the License for the 
+ * specific language governing permissions and limitations 
+ * under the License.
+ */
 
 import java.util.List;
 import java.util.Set;
 
-
-public class Source {
+/**
+ * This class rappresent the source tag available in the configuration tree 
+ * of the maven plugin
+ * 
+ * @author Anders Hessellund Jensen <ahj@trifork.com>
+ * @version $Id$
+ */
+public class Source
+{
 
     /**
      * Active the generation of java source compatibile with jdk previus 1.4
-     *
+     * 
      * @parameter compatible
      */
     private Boolean compatible = Boolean.TRUE;
-	
+
     /**
      * Whether the compiler should emit client stubs. Defaults to true.
-     *
+     * 
      * @parameter emitStubs;
      */
     private Boolean emitStubs = Boolean.TRUE;
 
     /**
      * Whether the compiler should emit server skeletons. Defaults to true.
-     *
+     * 
      * @parameter emitSkeletons;
      */
     private Boolean emitSkeletons = Boolean.TRUE;
 
     /**
      * Specifies a single, global packageprefix to use for all modules.
-     *
+     * 
      * @parameter packagePrefix;
      */
     private String packagePrefix;
 
     /**
      * Specifies which files to include in compilation.
-     *
+     * 
      * @parameter includes;
      */
     private Set includes;
 
     /**
      * Specifies which files to exclude from compilation.
-     *
+     * 
      * @parameter excludes;
      */
     private Set excludes;
 
     /**
      * The list of package prefixes for certain types.
-     *
+     * 
      * @parameter packagePrefixes;
      */
     private List packagePrefixes;
@@ -82,40 +91,76 @@ public class Source {
      */
     private List additionalArguments;
 
-
-    public List getDefines() {
+    /**
+     * 
+     * @return a <code>List</code> with all the defines with this source
+     */
+    public List getDefines()
+    {
         return defines;
     }
 
-    public Boolean emitStubs() {
+    /**
+     * @return a <code>Boolean</code> true if and only if the creation of the stubs is enabled
+     */
+    public Boolean emitStubs()
+    {
         return emitStubs;
     }
 
-    public Boolean emitSkeletons() {
+    /**
+     * @return a <code>Boolean</code> true if and only if the creation of the skeleton is enabled
+     */
+    public Boolean emitSkeletons()
+    {
         return emitSkeletons;
     }
-    
-    public Boolean compatible() {
-	return compatible;
+
+    /**
+     * @return a <code>Boolean</code> true if and only if the creation of compatible code is enabled
+     */
+    public Boolean compatible()
+    {
+        return compatible;
     }
 
-    public Set getExcludes() {
+    /**
+     * @return a <code>Set</code> with all the exclusions pattern
+     */
+    public Set getExcludes()
+    {
         return excludes;
     }
 
-    public Set getIncludes() {
+    /**
+     * @return a <code>Set</code> with all the inclusions pattern
+     */
+    public Set getIncludes()
+    {
         return includes;
     }
 
-    public String getPackagePrefix() {
+    /**
+     * @return the default package name that will be used as for all the generated classes
+     */
+    public String getPackagePrefix()
+    {
         return packagePrefix;
     }
 
-    public List getPackagePrefixes() {
+    /**
+     * @return a <code>List</code> of <code>PackagePrefix</code> to use for the generated files
+     */
+    public List getPackagePrefixes()
+    {
         return packagePrefixes;
     }
 
-    public List getAdditionalArguments() {
+    /**
+     * @return a <code>List</code> of <code>String</code> that will be added as compiler parameters
+     */
+    public List getAdditionalArguments()
+    {
         return additionalArguments;
     }
 }
